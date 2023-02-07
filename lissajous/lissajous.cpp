e1fe3ea4b42aa27d57c4095f15de1053151df211
Jay Parangalan <stellarfirefly@yahoo.com>
2023-02-06 18:28:28 -0800

    fix output, add output function

--- lissajous/lissajous.cpp
+++ lissajous/lissajous.cpp
@@ -2,38 +2,21 @@
 #include <algorithm>
 using namespace std;
 
-void blankGraph(int* graph, int xSize, int ySize) {
-    for (int y = 0; y < ySize; ++y) {
-        for (int x = 0; x < xSize; ++x) {
-            graph[y * xSize + x] = 0;
-        }
-    }
-}
-
-void setGraph(int* graph, int x, int y, int xSize) {
-    graph[y * xSize + x] = 1;
-}
-
-void plotGraph(int* graph, int xSize, int ySize) {
-    for (int y = 0; y < ySize; ++y) {
-        for (int x = 0; x < xSize; ++x) {
-            if (graph[y * xSize + x]) {
-                cout << "*";
-            }
-            else {
-                cout << " ";
-            }
-        }
-        cout << endl;
-    }
-}
-
 double poly(double x) {
-    while (abs(x) >= 0.1) {
-        x = poly(x / (sqrt(1 + x) + sqrt(1 - x)));
+    if (abs(x) < 0.1) {
+        x = x + pow(x, 3.0) / 6.0 + 0.75 * pow(x, 5.0) + pow(x, 7.0) / 22.4;
+        return x;
     }
-    x = x + pow(x, 3.0) / 6.0 + 0.75 * pow(x, 5.0) + pow(x, 7.0) / 22.4;
+    x = poly(x / (sqrt(1.0 + x) + sqrt(1.0 - x)));
+    x *= 2.0;
     return x;
+}
+
+void output(int tab) {
+    for (int i = 0; i < tab; ++i){
+        cout << " ";
+    }
+    cout << "*";
 }
 
 int main()
@@ -46,10 +29,10 @@ int main()
         // for a demo, try values: 5, 7, 0
     cout << "Relative frequency for X (integer): ";
     cin >> xFreq;
-    dxFreq = xFreq * 2 * PI;
+    dxFreq = (double) xFreq * 2.0 * PI;
     cout << "Relative frequency for Y (integer): ";
     cin >> yFreq;
-    dyFreq = yFreq * 2 * PI;
+    dyFreq = (double) yFreq * 2.0 * PI;
     cout << "Y phase as a multiple of pi (decimal): ";
     cin >> yPhase;
     yPhase *= PI;
@@ -60,17 +43,27 @@ int main()
         int* y = new int[2 * xFreq];
 
         for (int i = 0; i < xFreq; ++i) {
-            double t3 = (t1 + 2 * i * PI) / dxFreq;
-            double t4 = (t2 + 2 * i * PI) / dxFreq;
-            double y1 = 30 * sin(dyFreq * t3 + yPhase);
-            double y2 = 30 * sin(dyFreq * t4 + yPhase);
-            y[2 * i] = (int)round(y1);
-            y[2 * i + 1] = (int)round(y2);
+            double t3 = (t1 + 2.0 * (double)i * PI) / dxFreq;
+            double t4 = (t2 + 2.0 * (double)i * PI) / dxFreq;
+            double y1 = 30.0 * sin(dyFreq * t3 + yPhase);
+            double y2 = 30.0 * sin(dyFreq * t4 + yPhase);
+            y[2 * i] = (int)round(y1) + height * 2;
+            y[2 * i + 1] = (int)round(y2) + height * 2;
         }
         sort(y, y + xFreq * 2);
 
+        int prev;
         for (int i = 0; i < 2 * xFreq; ++i) {
-            cout << y[i]+36 << ", ";
+            if (i) {
+                int diff = y[i] - prev;
+                if (diff) {
+                    output(diff);
+                }
+            }
+            else {
+                output(y[i]);
+            }
+            prev = y[i];
         }
         cout << endl;
         delete[] y;
